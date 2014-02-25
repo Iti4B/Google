88004021aaa43db2a47aacc301e6475915dae541
Admin <admin@Andreas-iMac-2.local>
2014-02-25 16:03:24 +0100

    Initial Commit

---
+++ Toolbar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Toolbar
+//
+//  Created by Admin on 25/02/14.
+//  Copyright (c) 2014 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToolbarTests/ToolbarTests.m
@@ -0,0 +1,34 @@
+//
+//  ToolbarTests.m
+//  ToolbarTests
+//
+//  Created by Admin on 25/02/14.
+//  Copyright (c) 2014 Admin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ToolbarTests : XCTestCase
+
+@end
+
+@implementation ToolbarTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
